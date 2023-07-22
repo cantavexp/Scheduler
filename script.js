6ee206f6a7c1807ab8ecd402ccb49dcecbac076b
cantavexp <gunnadar@gmail.com>
2023-07-22 14:03:22 -0500

    push again

---
+++ script.js
@@ -0,0 +1,78 @@
+
+$(function () {
+   
+  $(".saveBtn").click(function () {
+    console.log(this);
+    console.log($(this).siblings(".description"));
+    console.log($(this).siblings(".description").val());
+    console.log($(this).parent().attr("id"));
+    
+    var time = $(this).parent().attr("id");
+    var text = $(this).siblings(".description").val();
+
+    localStorage.setItem(time, text);
+ 
+
+    $('.notification').addClass('show');
+  
+    setTimeout(function () {
+      $('.notification').removeClass('show');
+    }, 5000);
+    
+
+    // const userinput = $(this).siblings(".description").val()
+    // const containerid = $(this).parent().attr("id")
+    // localStorage.setItem(containerid, userinput)
+  });
+})
+  
+
+
+
+
+
+
+
+
+
+const timeBlocks = document.getElementsByClassName('time-block');
+for (let i = 0; i < timeBlocks.length; i++){
+  const timeBlock = timeBlocks[i];
+  const hour = parseInt(timeBlock.id.split('-'))[1];
+
+  if (hour < currentHour) {
+
+    timeBlock.classList.add('past');
+    
+    
+  } else if (hour === currentHour) {
+    timeBlock.classList.add('present');
+      
+  }  else {
+        timeBlock.classList.add('future');
+      }
+}
+
+
+
+
+  hourTracker();
+  setInterval(hourTracker, 15000);
+  
+  $("#hour-9").children(".description").val(localStorage.getItem("hour-9"))
+  $("#hour-10").children(".description").val(localStorage.getItem("hour-10"))
+  $("#hour-11").children(".description").val(localStorage.getItem("hour-11"))
+  $("#hour-12").children(".description").val(localStorage.getItem("hour-12"))
+  $("#hour-13").children(".description").val(localStorage.getItem("hour-13"))
+  $("#hour-14").children(".description").val(localStorage.getItem("hour-14"))
+  $("#hour-15").children(".description").val(localStorage.getItem("hour-15"))
+  $("#hour-16").children(".description").val(localStorage.getItem("hour-16"))
+  $("#hour-17").children(".description").val(localStorage.getItem("hour-17"))
+
+  $('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY'));
+
+ 
+
+  
+
+ 
